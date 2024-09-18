28accbca6dba91902f7af01eaceb8bc1b2920be5
Piska <you@piska.ru>
2024-09-18 08:48:40 +0300

    to-do-list

--- to-do-list.php
+++ to-do-list.php
@@ -1,51 +1,66 @@
 <?php
 $taskList = ["First task", "second task"];
 
-function showTask($inputTasks){
-    foreach ($inputTasks as $task) {
-        echo $task . "\n";
-    }
+function showTasks($inputTasks){
+foreach ($inputTasks as $task) {
+echo $task . "\n";
+}
 }
 
-function addTask(){
-    echo "To be implemented!\n";
+function addTask(&$inputTasks){
+$inputTasks[] = readline("Ievadiet uzdevumu: ");
+}
+
+function showTask($inputTasks){
+$taskIndex = readLine("Ievadiet uzdevuma ciparu: ");
+if(is_numeric($taskIndex)){
+echo $inputTasks[$taskIndex-1]. "\n";
+}else {
+echo "Nepareize ievade!\n";
+showTask($inputTasks);
+}
+
 }
 
 do {
-    echo "Uzdevumu parvaldnieks\n";
-    echo "Apskatīt visus uzdevumus => 1\n";
-    echo "Ievadīt jaunu uzdevumu => 2\n";
-    $choise = readline("Izvelaties darbību: ");
+echo "Uzdevumu parvaldnieks\n";
+echo "Izvadīt visu uzdevumu sarakstu => 1\n";
+echo "Ievadīt jaunu uzdevumu => 2\n";
+echo "Apskatīt vienu uzdevumu => 3\n";
+echo "Iziet ārā => 'N'";
+$choise = readline("Izvelaties darbību: ");
 
-    switch ($choise) {
-        case '1':
-            showTask($taskList);
-            break;
-        
-        case '2':
-            echo "To be implemented!\n";
-            break;
+switch ($choise) {
+case '1':
+showTasks($taskList);
+break;
 
-        default:
-            echo "Invalid option";
-            break;
-    }
+case '2':
+addTask($taskList);
+break;
+case '3':
+showTask($taskList);
+break;
+case 'N':
+break;
+default:
+echo "Invalid option";
+break;
+}
 
-    $loop = readline("Turpināt:\n");
-    if ($loop !="N") {
-       $array[] = "$loop"; 
-    }
-} while ($loop != "N");
+// $loop = readline("Turpināt:\n");
+// if ($loop !="N") {
+// $array[] = "$loop";
+// }
+} while ($choise != "N");
+print_r($taskList);
 
-var_dump($array);
-
-
-// 1.uzdevums
-//uztaisīt do-while cikls, ko lietotājs
-//var apstādināt ievadot "N"
-
-// 2.uzdevums
 /*
+1.uzdevums
+uztaisīt do-while cikls, ko lietotājs
+var apstādināt ievadot "N"
+
+2.uzdevums
 Izveidot switch-case konstruciju
 lai apstrādātu dažādas lietotāja ievadus
 
