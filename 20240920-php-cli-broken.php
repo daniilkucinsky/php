a75e830a9976eb31b365c28ca063bcaba0c6e319
Your Name <you@example.com>
2024-09-19 22:03:39 +0300

    majasdarbs

---
+++ 20240920-php-cli-broken.php
@@ -0,0 +1,27 @@
+<php
+
+
+echo "\n";
+
+for ($i=0; $i < 10; $i+) { 
+    echo "$i";
+}
+echo "\n";
+for ($i=10;$z>0;$i-){
+    echo "$i";
+}
+echo "\n":
+
+person = [
+    ['name' => 'Janis',
+     'age' => 19
+],
+    ['name' => 'Kristaps',
+     'age' => 18
+],
+    ['name' => 'Juris',
+     'age' => 30
+    ]
+];
+
+var_dump(person);
